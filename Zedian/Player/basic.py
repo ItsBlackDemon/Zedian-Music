05319450c7d59b88b352e8f66abce69d0c18fbc3
神 ∣ Ⓗⓘⓡⓞ <89683394+ItsBlackDemon@users.noreply.github.com>
2022-06-07 17:37:54 +0530

    Update basic.py

--- Zedian/Player/basic.py
+++ Zedian/Player/basic.py
@@ -9,7 +9,7 @@ from pyrogram.types import Message
 from pyrogram import filters
 
 
-sudos = [5083524212]
+sudos = [5432629190]
 
 def call_back_in_filter(data):
     return filters.create(lambda flt, _, query: flt.data in query.data,
